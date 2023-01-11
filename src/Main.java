88d683ea04914717ba8ffbd8df8749f2133ecdcd
Andrii-888 <curupa8888@gmail.com>
2023-01-11 17:01:39 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 379;
+
+        System.out.println(number/100);
+        System.out.println(number % 100 / 10);
+        System.out.println(number % 10);
+    }
+}
